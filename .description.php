1c4d3572491438b657a3b119b3a1b2c641e72b27
webmanana <lukrs@mail.ru>
2025-02-03 11:46:26 +0300

    add description

---
+++ .description.php
@@ -0,0 +1,16 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+$arModuleVersion = [
+    "VERSION" => "1.0.0",
+    "VERSION_DATE" => "2025-01-28"
+];
+
+$arModuleDescription = [
+    "MODULE_ID" => "my.currency",
+    "MODULE_NAME" => "Курсы валют",
+    "MODULE_DESCRIPTION" => "Модуль для работы с курсами валют",
+    "PARTNER_NAME" => "webmanana",
+    "PARTNER_URI" => "http://localhost:81/"
+];
+
+?>
